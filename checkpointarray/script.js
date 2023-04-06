f7b0eb2d849eb772df93288c9a64f5c87b9a8c4d
Bouras-Rayane <120200840+Bouras-Rayane@users.noreply.github.com>
2023-04-06 14:01:14 +0100

    Add files via upload

---
+++ checkpointarray/script.js
@@ -0,0 +1,31 @@
+//first problem
+set1 = [3, 1, 7, 9]
+set2 = [2, 4, 1, 9, 3]
+print(sumOfDistinctElements(set1, set2)) = 13
+
+//second problem
+function dot_product(v1, v2)
+ for [i] = 1 to (n),
+     input [v1] , [v2]
+     ps = dot_product(v1, v2)
+     if ps == [0]
+         print ("The vectors are orthogonal")
+     else
+         print ("The vectors are not orthogonal")
+
+     
+     
+     
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
