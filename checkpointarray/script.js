0011e976595e65f5decf9c895b79aa4a2bd0123f
Bouras-Rayane <120200840+Bouras-Rayane@users.noreply.github.com>
2023-04-08 20:32:16 +0100

    Add files via upload

--- checkpointarray/script.js
+++ checkpointarray/script.js
@@ -1,12 +1,20 @@
- insertion_sort(arr)
-    for [i] in range(1, len(arr))
-        key = arr[i]
-        j = i - 1
-        while [j] >= 0 arr [j] > key
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-    return arr
+const word = "racecar";
+if (isPalindrome(word)) {
+  console.log(`${word} is a palindrome.`);
+} else {
+  console.log(`${word} is not a palindrome.`);
+}
+  
+ 
+ function isPalindrome(word) {
+   if (word.length <= 1) {
+     return true;
+   } else if (word[0] === word[word.length - 1]) {
+     return isPalindrome(word.slice(1, -1));
+   } else {
+     return false;
+   }
+ }
 
 
 
